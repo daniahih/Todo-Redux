f1b44c7ebf61d1162f94c4cc9b8d1d0fca0b4125
daniahih <daniahih310@gmail.com>
2023-05-08 08:57:53 +0300

    added rtk-qyery task

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { todoImgApi } from "../api/apiSlice";
 import todoReducer from "./todoSlice";
 
 export default configureStore({
   reducer: {
     todos: todoReducer,
+    [todoImgApi.reducerPath]: todoImgApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todoImgApi.middleware),
 });
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { deleteTodo } from "../Store/todoSlice";
+import { useGetRandomImgQuery } from "../api/apiSlice";
 
 const TodoList = () => {
-  const todos = useSelector((state) => state.todos);
+  const todos = useSelector((state) => state.todos); // it return the todos slice from state object
   const dispatch = useDispatch();
+  const [checkedTodo, setcheckedTodo] = useState([]);
+  const { refetch } = useGetRandomImgQuery();
+  const [checkedImg, setCheckedImg] = useState();
 
   useEffect(() => {
     if (todos.length === 0) {
@@ -19,11 +23,31 @@ const TodoList = () => {
     dispatch(deleteTodo(id));
   };
 
+  function handleCheckboxChange(id) {
+    if (checkedTodo.includes(id)) {
+      setcheckedTodo(checkedTodo.filter((checkedId) => checkedId !== id));
+    } else {
+      refetch().then((result) => {
+        const updatedData = result.data;
+        console.log(updatedData);
+        setCheckedImg(updatedData.htmlCode);
+        setcheckedTodo([...checkedTodo, id]);
+      });
+    }
+  }
   return (
     <div>
       <h2>Todo List</h2>
       {todos.map((todo) => (
         <div key={todo.id}>
+          <input
+            type="checkbox"
+            checked={checkedTodo.includes(todo.id)}
+            onChange={() => handleCheckboxChange(todo.id)}
+          />
+          {checkedTodo.includes(todo.id) && (
+            <span dangerouslySetInnerHTML={{ __html: checkedImg }}></span>
+          )}
           <span>{todo.title}</span>
           <button onClick={() => handleDelete(todo.id)}>Delete</button>
         </div>
@@ -31,5 +55,4 @@ const TodoList = () => {
     </div>
   );
 };
-
 export default TodoList;
